1f7fd3d2f1c161dd8259ec4aaf04b62a5db5c307
tyrik1010 <tyrik.chen@gmail.com>
2024-06-18 13:38:21 +0800

    fix js

--- Surge/module/js/Ip-Info.js
+++ Surge/module/js/Ip-Info.js
@@ -18,23 +18,23 @@ let content = ''
 	let [info] = await Promise.all([getInfo()])
 	$.log($.toStr(info))
 	const ip = $.lodash_get(info, 'ip') || ' - '
-	let ipif = 'IP地址:     ' + `${ip}\n`
+	let ipif = 'IP地址:' + `${ip}\n`
 	let geo = [];
 	['country', 'city'].forEach(key => {
 		geo.push(`${$.lodash_get(info, `location.${key}`) || ' - '}`)
 	})
 	geo = geo.length > 0 ? `${geo.join(' ')}\n` : ''
-	geo = 'IP位置:     ' + geo
+	geo = 'IP位置:' + geo
 	let company = [];
 	['name'].forEach(key => {
 		company.push(
-			`企业${key === 'name' ? '' : ` ${key.toUpperCase()}`}:   ${$.lodash_get(info, `company.${key}`) || ' - '}`
+			`企业${key === 'name' ? '' : ` ${key.toUpperCase()}`}:     ${$.lodash_get(info, `company.${key}`) || ' - '}`
 		)
 	})
 	let asn = [];
 	['org'].forEach(key => {
 		asn.push(
-			`ASN${key === 'org' ? '' : ` ${key.toUpperCase()}`}: ${$.lodash_get(info, `asn.${key}`) || ' - '}`
+			`ASN${key === 'org' ? '' : ` ${key.toUpperCase()}`}:     ${$.lodash_get(info, `asn.${key}`) || ' - '}`
 		)
 	})
 	asn = asn.length > 0 ? `${asn}\n` : ''
@@ -45,7 +45,7 @@ let content = ''
 		)
 	})
 	type = type.length > 0 ? `${type}` : 'hosting'
-	let asnTs = type.substring(7)
+	let asnTs = type.substring(type.indexOf(' '))
 	if('hosting' === asnTs) {
 		type = type.replace(asnTs, '机房IP')
 	} else if ('isp' === asnTs) {
