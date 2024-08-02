7e99bdd2a9568923a78c15b6c258fd08979bc361
ZEUS <tyrik.chen@gmail.com>
2024-08-02 10:17:48 +0800

    Update IpInfo.js
    fix js

--- Surge/module/js/IpInfo.js
+++ Surge/module/js/IpInfo.js
@@ -132,7 +132,7 @@ async function getCnText() {
 		const response = await $.http.get({
 			url: `https://ping0.cc/geo/`,
 			headers: {
-				Referer: 'https://ping0.cc/geo/',
+				Referer: `https://ping0.cc/geo/',
 				'User-Agent': 'Mozilla/5.0 (iPhone CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1 Edg/109.0.0.0',
 			},
 		})
