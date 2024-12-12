b4e1eb5345d2c680c7f0cf13c475db0740657d76
tyrik1010 <tyrik.chen@gmail.com>
2024-12-12 12:15:06 +0800

    fix

--- Surge/module/js/SpotifyJson.js
+++
@@ -1,9 +0,0 @@
-let url = $request.url;
-if (url.includes('platform=iphone')) {
-    url = url.replace(/platform=iphone/, 'platform=ipad');
-} else {
-    console.log('无需处理');
-}
-$done({
-    url
-});
